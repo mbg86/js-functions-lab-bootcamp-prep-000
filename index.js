cc823128feabd79580a64d658040dfada006298e
Manrique Buso - Garcia <manrique.buso@gmail.com>
2018-10-23 02:54:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function happyHolidays(string) {
   return('Happy holidays!')
 }
 
-function say happyHolidaysTo(string) {
+function happyHolidaysTo(string) {
   return("Happy holidays, ${name}!")
 }
 
